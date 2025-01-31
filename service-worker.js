e6a886bcca7da9a2bd89cd0f3727c4ca28f85645
Bono <bono.tadic@porsche.digital>
2025-01-31 01:06:52 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 const CACHE_NAME = 'qr-scanner-v1';
-const OFFLINE_URL = 'images/offline.png';
 const ASSETS = [
     '/',
     '/index.html',
     '/styles/main.css',
     '/scripts/main.js',
+    '/manifest.json',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    OFFLINE_URL
+    '/images/offline.png'
 ];
 
 self.addEventListener('install', event => {
@@ -21,8 +21,39 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
-            .catch(() => caches.match(OFFLINE_URL))
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(networkResponse => {
+                        return caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, networkResponse.clone());
+                                return networkResponse;
+                            });
+                    })
+                    .catch(() => {
+                        if (event.request.mode === 'navigate') {
+                            return caches.match('/index.html');
+                        }
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
 });
 
@@ -31,7 +62,7 @@ self.addEventListener('sync', event => {
         event.waitUntil(
             self.registration.showNotification('Sync Complete', {
                 body: 'Data has been synchronized.',
-                icon: 'icons/icon-192x192.png'
+                icon: '/icons/icon-192x192.png'
             })
         );
     }
@@ -45,14 +76,14 @@ self.addEventListener('push', event => {
         data = {
             title: 'New Message',
             body: event.data.text(),
-            icon: 'icons/icon-192x192.png'
+            icon: '/icons/icon-192x192.png'
         };
     }
 
     const title = data.title || 'New Message';
     const options = {
         body: data.body || 'You have a new message!',
-        icon: data.icon || 'icons/icon-192x192.png'
+        icon: data.icon || '/icons/icon-192x192.png'
     };
 
     event.waitUntil(
